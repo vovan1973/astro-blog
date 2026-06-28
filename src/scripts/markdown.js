4a9879581ffd4172ff3c9c59c4ae6efa53a3629a
Vovan1973 <teresvovan1973@gmail.com>
2026-06-28 16:29:39 +0300

    Создаем ссилку на маркдов

---
+++ src/scripts/markdown.js
@@ -0,0 +1,12 @@
+
+// делаем виборку
+const container= document.querySelector('#container');
+const source= document.querySelector('#source');
+const preview= document.querySelector('#preview');
+const divider= document.querySelector('#divider');
+
+// создаем событие при движении мыши
+source.addEventListener('mousemove', () => {
+      const newText= marked.parse(source.value);
+      preview.innerHTML= newText;
+});
