c7d6ed91be136fa3304a5d59439e458b3959bd63
Vovan1973 <teresvovan1973@gmail.com>
2026-06-28 20:25:07 +0300

    Загрузить github pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,12 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+// import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+// export default defineConfig({});
+
+import { defineConfig } from 'astro/config';
+
+export default defineConfig({
+  site: 'https://github.com/vovan1973/astro-blog.github.io',
+  base: '/astro-blog'
+});
