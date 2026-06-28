e7bf6c49bbee669ffa7fe0c2f328786eafcbfefe
Vovan1973 <teresvovan1973@gmail.com>
2026-06-28 16:33:52 +0300

    Изменнения для миши

--- src/scripts/markdown.js
+++ src/scripts/markdown.js
@@ -6,7 +6,7 @@ const preview= document.querySelector('#preview');
 const divider= document.querySelector('#divider');
 
 // создаем событие при движении мыши
-source.addEventListener('mousemove', () => {
+source.addEventListener('keyup', () => {
       const newText= marked.parse(source.value);
       preview.innerHTML= newText;
 });
